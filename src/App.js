8c59200a2aaed5fe44ffdbe559feef9f2bc3c09c
Maitrimodii <102842129+Maitrimodii@users.noreply.github.com>
2024-02-14 10:11:12 +0530

    inital commnit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Nav from './Components/NavBar';
-import Hero from './Components/Hero';
-import Card1 from './Components/Card1';
-import Card2 from './Components/Card2';
-import Testimonals from './Components/Testimonals';
-import Footer from './Components/Footer';
-import GroupImage from './Components/GroupImage';
+import "./App.css";
+import Nav from "./Components/NavBar";
+import Hero from "./Components/Hero";
+import Card1 from "./Components/Card1";
+import Testimonals from "./Components/Testimonals";
+import Footer from "./Components/Footer";
+import GroupImage from "./Components/GroupImage";
+import AddPage from "./Components/AddPage";
 
 function App() {
   return (
-   <div>
-    <Nav/>
-    <Hero/>
-    <Card1/>
-    <Card2/>
-    <Testimonals/>
-    <GroupImage/>
-    <Footer/>
-   </div>
+    <div>
+      <Nav />
+      <Hero />
+      <Card1 />
+      <AddPage />
+      <Testimonals />
+      <GroupImage />
+      <Footer />
+    </div>
   );
 }
 
